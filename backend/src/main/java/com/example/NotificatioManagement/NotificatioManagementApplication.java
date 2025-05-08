c55b3228407e199d6517e6c1d0b9980f25ae90c1
Sandali Dahanayaka <162104817+snsdahanayaka@users.noreply.github.com>
2025-05-08 23:38:28 +0530

    initial

---
+++ backend/src/main/java/com/example/NotificatioManagement/NotificatioManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.NotificatioManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificatioManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificatioManagementApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/NotificatioManagement/NotificatioManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotificatioManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificatioManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
